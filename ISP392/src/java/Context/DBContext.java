5c1cb9ec90371ca6d59dcc6c30adcf23625c91ec
longv0208 <hailongv2004@gmail.com>
2024-10-23 09:05:23 +0700

    update details classroom

--- ISP392/src/java/Context/DBContext.java
+++ ISP392/src/java/Context/DBContext.java
@@ -24,8 +24,8 @@ public class DBContext {
         
         try {
             String user = "sa";
-            String pass = "123456";
-            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=TEST";
+            String pass = "123";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=ISP";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             return DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
