43b18b71a881c862ba9fb7fbfdaa8d010588434e
doubled58 <163427477+doubled58@users.noreply.github.com>
2024-11-01 13:14:08 +0700

    Add files via upload

---
+++ ISP392/src/java/models/StudentClass.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Dell
+ */
+public class StudentClass {
+    private String classId;
+    private String studentId;
+    private String studentName; 
+
+    public StudentClass() {
+    }
+
+    public StudentClass(String classId, String studentId, String studentName) {
+        this.classId = classId;
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    
+
+
+ 
+}
