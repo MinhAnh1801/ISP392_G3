7e7c5b107d667138e7e4fdb8e10c95a813f2bebe
doubled58 <163427477+doubled58@users.noreply.github.com>
2024-11-07 00:36:03 +0700

    Add files via upload

---
+++ ISP392/src/java/Model/LecturerProfile.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author ADMIN-PC
+ */
+public class LecturerProfile {
+
+    private int lecturerId;
+    private String fullName;
+    private boolean hasFeedback;
+
+    public LecturerProfile() {
+    }
+
+    public LecturerProfile(int lecturerId, String fullName) {
+        this.lecturerId = lecturerId;
+        this.fullName = fullName;
+    }
+
+    public int getLecturerId() {
+        return lecturerId;
+    }
+
+    public void setLecturerId(int lecturerId) {
+        this.lecturerId = lecturerId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public boolean isHasFeedback() {
+        return hasFeedback;
+    }
+
+    public void setHasFeedback(boolean hasFeedback) {
+        this.hasFeedback = hasFeedback;
+    }
+
+}
